e4d07d842c74ea7d91d27d6989d238222eb933c8
Lukas Scheffler <lukas.scheffler@physik.uni-wuerzburg.de>
2018-10-02 11:14:24 +0200

    fix of issue #11 of python-tmcl

--- pyTMCL/bus.py
+++ pyTMCL/bus.py
@@ -19,87 +19,89 @@ REPLY_LENGTH_IIC = 8
 
 class Bus (object):
 
-	def __init__( self, serial, CAN = False ):
-		self.CAN = CAN
-		self.serial = serial
+    def __init__(self, serial, CAN=False):
+        self.CAN = CAN
+        self.serial = serial
 
-	def send ( self, address, command, type, motorbank, value ):
-		"""
-		Send a message to the specified module.
-		This is a blocking function that will not return until a reply
-		has been received from the module.
-		
-		See the TMCL docs for full descriptions of the parameters
-		
-		:param address:   Module address to send command to
-		:param command:   Instruction no
-		:param type:	  Type
-		:param motorbank: Mot/Bank
-		:param value:	  Value
-	
-		:rtype: Reply
-		"""
-		if self.CAN:
-			msg = struct.pack(MSG_STRUCTURE_CAN, command, type, motorbank,value)
-			self.serial.write(msg)
-			resp = [0]
-			data = self.serial.read(REPLY_LENGTH_CAN)
-			resp.extend(struct.unpack(REPLY_STRUCTURE_CAN, data))
-			reply = Reply(resp)
-			return self._handle_reply(reply)
-		else:
-			checksum = self._binaryadd(address, command, type, motorbank, value)
-			msg = struct.pack(MSG_STRUCTURE, address, command, type, motorbank, value, checksum)
-			self.serial.write(msg)
-			rep = self.serial.read(REPLY_LENGTH)
-			reply = Reply(struct.unpack(REPLY_STRUCTURE, rep))
-			return self._handle_reply(reply)
+    def send(self, address, command, type, motorbank, value):
+        """
+        Send a message to the specified module.
+        This is a blocking function that will not return until a reply
+        has been received from the module.
 
-	def get_module( self, address=1 ):
-		"""
-			Returns a Module object targeting the device at address `address`
-			You can use this object to retrieve one or more axis motors.
+        See the TMCL docs for full descriptions of the parameters
 
-			:param address:  
-				Bus address to the target TMCM module
-			:type  address: int
+        :param address:   Module address to send command to
+        :param command:   Instruction no
+        :param type:	  Type
+        :param motorbank: Mot/Bank
+        :param value:	  Value
 
-			:rtype: Module
-		"""
-		return Module(self, address)
+        :rtype: Reply
+        """
+        if self.CAN:
+            msg = struct.pack(MSG_STRUCTURE_CAN, command,
+                              type, motorbank, value)
+            self.serial.write(msg)
+            resp = [0]
+            data = self.serial.read(REPLY_LENGTH_CAN)
+            resp.extend(struct.unpack(REPLY_STRUCTURE_CAN, data))
+            reply = Reply(resp)
+            return self._handle_reply(reply)
+        else:
+            checksum = self._binaryadd(
+                address, command, type, motorbank, value)
+            msg = struct.pack(MSG_STRUCTURE, address, command,
+                              type, motorbank, value, checksum)
+            self.serial.write(msg)
+            rep = self.serial.read(REPLY_LENGTH)
+            reply = Reply(struct.unpack(REPLY_STRUCTURE, rep))
+            return self._handle_reply(reply)
 
-	def get_motor( self, address=1, motor_id=0 ):
-		"""		
-			Returns object addressing motor number `motor_id` on module `address`.
-			`address` defaults to 1 (doc for TMCM310 starts counting addresses at 1).
-			`motor_id` defaults to 0 (1st axis).
+    def get_module(self, address=1):
+        """
+                Returns a Module object targeting the device at address `address`
+                You can use this object to retrieve one or more axis motors.
 
-			This is an alias for `get_module(address).get_motor(motor_id)` so that 
-			backward-compatibility with v1.1.1 is maintained.
+                :param address:
+                        Bus address to the target TMCM module
+                :type  address: int
 
-			:param address:  
-				Bus address of the target TMCM module
-			:type  address:  int
+                :rtype: Module
+        """
+        return Module(self, address)
 
-			:param motor_id:
-				ID of the motor/axis to target
-			:type  motor_id: int
+    def get_motor(self, address=1, motor_id=0):
+        """
+                Returns object addressing motor number `motor_id` on module `address`.
+                `address` defaults to 1 (doc for TMCM310 starts counting addresses at 1).
+                `motor_id` defaults to 0 (1st axis).
 
-			:rtype: Motor
-		"""
-		return Motor(self, address, motor_id)
+                This is an alias for `get_module(address).get_motor(motor_id)` so that
+                backward-compatibility with v1.1.1 is maintained.
 
-	def _handle_reply (self, reply):
-		if reply.status < Reply.Status.SUCCESS:
-			raise TrinamicException(reply)
-		return reply
+                :param address:
+                        Bus address of the target TMCM module
+                :type  address:  int
 
-	def _binaryadd( self, address, command, type, motorbank, value ):
-		checksum_struct = struct.pack(MSG_STRUCTURE[:-1], address, command, type, motorbank, value)
-		checksum = 0
-		for s in checksum_struct:
-			checksum += int(s) % 256
-			checksum = checksum % 256
-		return checksum
+                :param motor_id:
+                        ID of the motor/axis to target
+                :type  motor_id: int
 
+                :rtype: Motor
+        """
+        return Motor(self, address, motor_id)
 
+    def _handle_reply(self, reply):
+        if reply.status < Reply.Status.SUCCESS:
+            raise TrinamicException(reply)
+        return reply
+
+    def _binaryadd(self, address, command, type, motorbank, value):
+        checksum_struct = struct.pack(
+            MSG_STRUCTURE[:-1], address, command, type, motorbank, value)
+        checksum = 0
+        for s in checksum_struct:
+            checksum += int(s.encode('hex'), 16) % 256
+            checksum = checksum % 256
+        return checksum
