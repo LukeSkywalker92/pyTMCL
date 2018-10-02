97484ecb1098e3a2c7b2d7b9efc6e906b6e80469
Lukas Scheffler <lukas.scheffler@physik.uni-wuerzburg.de>
2018-10-02 11:13:54 +0200

    renamed module

--- examples/example1.py
+++ examples/example1.py
@@ -1,28 +1,26 @@
 from serial import Serial
-import TMCL
+import pyTMCL
 
 
-def simple_example ():
-	## Open a serial port to connect to the bus
-	serial_port = Serial("/dev/tty.usbmodem1241")
-	bus = TMCL.connect(serial_port)
+def simple_example():
+    # Open a serial port to connect to the bus
+    serial_port = Serial("/dev/tty.usbmodem1241")
+    bus = pyTMCL.connect(serial_port)
 
-	## Get the motor on axis 0 of module with address 1
-	motor = bus.get_motor(1)
-
-
+    # Get the motor on axis 0 of module with address 1
+    motor = bus.get_motor(1)
 
 
 def example_for_multi_axis_modules():
-	## Open a serial port to connect to the bus
-	serial_port = Serial("/dev/tty.usbmodem1241")
-	bus = TMCL.connect(serial_port)
+    # Open a serial port to connect to the bus
+    serial_port = Serial("/dev/tty.usbmodem1241")
+    bus = pyTMCL.connect(serial_port)
 
-	## Get the module at address 1
-	module = bus.get_module(1)
+    # Get the module at address 1
+    module = bus.get_module(1)
 
-	## Get the motor on axis '0' of the module
-	motor = module.get_motor(0)
+    # Get the motor on axis '0' of the module
+    motor = module.get_motor(0)
 
-	## Start sending commands to the motor
-	motor.stop()
+    # Start sending commands to the motor
+    motor.stop()
