cd39f4fb56288ff30883c184ec217c20211b53f8
Lukas Scheffler <lukas.scheffler@physik.uni-wuerzburg.de>
2018-10-04 16:23:52 +0200

    ready for publication

--- pyTMCL/__init__.py
+++ pyTMCL/__init__.py
@@ -2,6 +2,8 @@ from .bus import Bus
 from .motor import Motor
 from .commands import Command
 from .reply import Reply
+from .trigger_thread import TriggerThread
 
-def connect ( serial_port, CAN = False ):
+
+def connect(serial_port, CAN=False):
     return Bus(serial_port, CAN)
--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='pyTMCL',
       version='1.0.0',
       description='Talk to Trinamic Stepper Motors using TMCL over serial',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/LukeSkywalker92/pyTMCL',
-      author='Alan Pich',
+      author='Lukas Scheffler',
       author_email='luke@lukecodewalker.de',
       license='MIT',
+      classifiers=[
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Development Status :: 5 - Production/Stable",
+          "Intended Audience :: Developers",
+          "Natural Language :: English",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
       packages=['pyTMCL'],
       install_requires=[
           'pyserial'
