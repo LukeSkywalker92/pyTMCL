44e504b74a75182751bb306d74374f8806ad4d08
Lukas Scheffler <luke@lukecodewalker.de>
2018-10-06 17:04:42 +0200

    1.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pyTMCL',
-      version='1.0.1',
+      version='1.0.2',
       description='Talk to Trinamic Stepper Motors using TMCL over serial',
       long_description=long_description,
       long_description_content_type="text/markdown",
